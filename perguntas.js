97fd6ef0e63117bd66ba227448d3f8e07d064203
gabizinha-23 <stankievicz.gabriela@escola.pr.gov.br>
2025-09-17 13:20:20 +0000

    salvar

---
+++ perguntas.js
@@ -0,0 +1,45 @@
+criaCartao(
+    'Promagração,'
+    'O que é HTML,'
+    resposta
+)
+criaCartao(
+    'Promagração,'
+    'O que é CSS,'
+    resposta
+)
+criaCartao(
+    'Promagração,'
+    'O que é JS,'
+    resposta
+)
+criaCartao(
+    'Promagração,'
+    'Pra que serve o ARTICLE,'
+    resposta
+)
+criaCartao(
+    'Promagração,'
+    pergunta,
+    resposta
+)
+criaCartao(
+    'Promagração,'
+    pergunta,
+    resposta
+)
+criaCartao(
+    'Promagração,'
+    pergunta,
+    resposta
+)
+criaCartao(
+    'Promagração,'
+    pergunta,
+    resposta
+)
+criaCartao(
+    'Promagração,'
+    pergunta,
+    resposta
+)
